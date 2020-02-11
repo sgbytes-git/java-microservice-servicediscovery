bbe9995da46462850bfa17593d2e2cb73dc0fe1b
sgbytes-git <keerthiramraj@gmail.com>
2020-02-11 10:56:36 +0800

    Initial Commit

---
+++ src/test/java/com/infra/dev/servicediscovery/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infra.dev.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
