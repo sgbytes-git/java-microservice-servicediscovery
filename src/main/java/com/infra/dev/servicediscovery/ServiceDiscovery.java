194c46d083af1740d874b18ab1ee0a8368e5adca
sgbytes-git <keerthiramraj@gmail.com>
2020-02-14 10:03:47 +0800

    authentication changes

--- src/main/java/com/infra/dev/servicediscovery/ServiceDiscovery.java
+++ src/main/java/com/infra/dev/servicediscovery/ServiceDiscovery.java
@@ -3,8 +3,10 @@ package com.infra.dev.servicediscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaServer
+@EnableZuulProxy
 @SpringBootApplication
 public class ServiceDiscovery {
 
---
+++ src/main/java/com/infra/dev/servicediscovery/config/AuthorizationServerConfig.java
@@ -0,0 +1,55 @@
+package com.infra.dev.servicediscovery.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    static final String GRANT_TYPE = "password";
+    static final String SCOPE_READ = "read";
+    static final String SCOPE_WRITE = "write";
+    static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1 * 60 * 60;
+    static final int REFRESH_TOKEN_VALIDITY_SECONDS = 6 * 60 * 60;
+
+    @Value(value="${spring.oauth.client-id}")
+    private String clientId;
+
+    @Value(value="${spring.oauth.client-secret}")
+    private String clientSecret;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
+
+        configurer
+                .inMemory()
+                .withClient(clientId)
+                .secret(clientSecret)
+                .authorizedGrantTypes(GRANT_TYPE)
+                .scopes(SCOPE_READ, SCOPE_WRITE)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).
+                refreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY_SECONDS);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(tokenStore)
+                .authenticationManager(authenticationManager);
+    }
+}
